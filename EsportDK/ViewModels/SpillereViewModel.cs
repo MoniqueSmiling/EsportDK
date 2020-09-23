a60acdbc7ae5e31f46e51fe47bcea4486ea82b02
Lola Jahn <lolajahn@gmail.com>
2020-09-23 11:56:31 +0200

    Add project files.

---
+++ EsportDK/ViewModels/SpillereViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace EsportDK.ViewModels
+{
+    class SpillereViewModel
+    {
+    }
+}
---
+++ EsportDK/Views/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using EsportDK.ViewModels;
+using System.Windows;
+
+namespace EsportDK
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void spillereMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new SpillereViewModel();
+        }
+
+        private void turneringerMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new TurneringerViewModel();
+        }
+
+        private void ansatteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new AnsatteViewModel();
+        }
+        private void sponsorerMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new SponsorerViewModel();
+        }
+    }
+}
